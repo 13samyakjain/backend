4492331fb337526d2d2e357c2f6b03cf751edc75
Samyak Jain <samyakjain@Samyaks-MacBook-Air.local>
2023-04-06 20:52:08 +0530

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5005;
+
+app.get('/', (req, res) => {
+  res.send({ id: 1 });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
