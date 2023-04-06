d8bdd94876589e03ba8cf82c662ec68c9c3af5ca
Samyak Jain <57790761+13samyakjain@users.noreply.github.com>
2023-04-06 21:09:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// backend.js
+
+// This is a dummy backend code that simply returns a response object
+// with a predefined message and timestamp.
+
 const express = require('express');
 const app = express();
-const port = 5005;
 
 app.get('/', (req, res) => {
-  res.send({ id: 1 });
+  const response = {
+    message: 'Hello from the backend!',
+    timestamp: new Date().toISOString()
+  };
+  res.json(response);
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
